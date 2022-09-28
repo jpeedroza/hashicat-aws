83cd28842604d63b37e96a2535d40597dbe771d2
Joao Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-09-28 16:45:12 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -128,6 +129,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
   }
 }
 
