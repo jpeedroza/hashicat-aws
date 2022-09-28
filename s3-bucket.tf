8d813a795c6b9227664649b666749ea44de018ae
Joao Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-09-28 17:23:00 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,5 @@ module "s3_bucket" {
   source  = "app.terraform.io/pedroza-org/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = var.prefix
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
+  bucket_prefix = var.prefix
 }
